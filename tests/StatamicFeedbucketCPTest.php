745be88c06f541c5b0c5167682cce148e5c7d57c
Sami Walbury <sami@steadfastcollective.com>
2025-08-13 15:19:49 +0100

    Resolve issue with inlineScripts not being cleared, and tidied up Feedbucket test

--- tests/StatamicFeedbucketCPTest.php
+++ tests/StatamicFeedbucketCPTest.php
@@ -3,10 +3,6 @@
 namespace SteadfastCollective\StatamicFeedbucket\Tests;
 
 use Statamic\Facades\User;
-use Statamic\Facades\Stache;
-use Statamic\Facades\GlobalSet;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Route;
 use Statamic\Testing\Concerns\PreventsSavingStacheItemsToDisk;
 
 class StatamicFeedbucketCPTest extends TestCase
@@ -18,48 +14,32 @@ class StatamicFeedbucketCPTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
-        $this->createGlobal();
-        // Cache::fake();
-        // Stache::fake();
-        // Cache::flush();
-        // Stache::clear();
-
-        // if (app()->configurationIsCached()) {
-        //     \Illuminate\Support\Facades\Artisan::call('config:clear');
-        // }
+        $this->clearStatamicInlineScripts();
     }
 
     public function test_feedbucket_is_included_if_page_is_in_config(): void
     {
-        config([
-            'statamic-feedbucket.cms_routes' => ['statamic.cp.dashboard'],
-            'app.env' => 'local'
-        ]);
+        $this->setGlobal();
 
         $user = User::make()->makeSuper();
         $user->save();
         $this->actingAs($user);
+
+        config([
+            'statamic-feedbucket.cms_routes' => ['statamic.cp.dashboard'],
+            'app.env' => 'local'
+        ]);
 
         $request = $this->get('/cp/dashboard');
 
         $request->assertOK();
         $request->assertSee($this->feedbucketString);
-
-        config([
-            'statamic-feedbucket.cms_routes' => ['statamic.cp.dashboard'],
-            'app.env' => 'local'
-        ]);
-
-        $request = $this->get('/cp/collections');
-
-        ray($request->getContent());
-
-        $request->assertOK();
-        $request->assertDontSee($this->feedbucketString);
     }
 
     public function test_feedbucket_is_not_included_if_page_is_not_in_config(): void
     {
+        $this->setGlobal();
+
         config([
             'statamic-feedbucket.cms_routes' => ['statamic.cp.dashboard'],
             'app.env' => 'local'
@@ -71,29 +51,7 @@ class StatamicFeedbucketCPTest extends TestCase
 
         $request = $this->get('/cp/collections');
 
-        ray($request->getContent());
-
         $request->assertOK();
         $request->assertDontSee($this->feedbucketString);
-    }
-
-    private function createGlobal(): void
-    {
-
-        $global = GlobalSet::make('feedbucket');
-        $variables = $global->makeLocalization('default');
-
-        $variables->data([
-            'enable_in_cms' => true,
-            'feedbucket_id' => 'abc123',
-            'enabled_environments' => [
-                'local' => true,
-                'staging' => false,
-                'production' => false,
-            ],
-        ]);
-
-        $global->addLocalization($variables);
-        $global->save();
     }
 }
