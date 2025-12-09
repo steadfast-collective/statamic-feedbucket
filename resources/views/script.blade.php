625d6cb4f529ee5976fd90f9adbf2cf7e098455f
James Pigott <james.pigott@steadfastcollective.com>
2025-12-09 12:14:24 +0000

    Replace partial with tag

---
+++ resources/views/script.blade.php
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+    (function(k,s) {
+        s=document.createElement('script');s.module=true;s.async=true;
+        s.src="https://cdn.feedbucket.app/assets/feedbucket.js";
+        s.dataset.feedbucket=k;document.head.appendChild(s);
+    })('{{ $id }}')
+</script>
